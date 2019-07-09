ae61192e4a98187acb49402a6ee6bbfa6e14f5f1
teakongnoo <tea.kongnoo@gmail.com>
2019-07-09 16:02:02 +0700

    Add existing file

--- bot.php
+++ bot.php
@@ -21,6 +21,11 @@ if ( sizeof($request_array['events']) > 0 ) {
         $reply_token = $event['replyToken'];
 
         $text = $event['message']['text'];
+//test///
+if($text="test"){
+    $text = "Yes";
+}
+
         $data = [
             'replyToken' => $reply_token,
             // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
